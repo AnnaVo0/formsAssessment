9aff1bd3b35ee8d7bd7afe59a41c5d7d76bcef22
Anna Vo <avo12@mail.valenciacollege.edu>
2024-03-10 16:07:30 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// ul reference
+const ul = document.querySelector("ul");
+
+// todo list array
+let toDoList = [];
+
+// adds a todo to todo list on form submission
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let toDo = document.querySelector("#toDo").value;
+    toDoList.push(toDo);
+
+    // Maps array items into todo list items
+    const template = toDoList.map(toDoListItem => `
+        <li>${toDoListItem}</li>
+    `);
+
+    ul.innerHTML = template.join("");
+});
+
